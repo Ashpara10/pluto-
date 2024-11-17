e7aff7403ca81be2272b3dcf05c767a388df2a33
AshPara10 <ashwinparande1156610c@gmail.com>
2024-11-17 17:59:04 +0530

    uuid validation added

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -33,7 +33,7 @@ export async function getArticleDataByURL(url: string) {
   return bodyHtml.html();
 }
 
-function isValidUUID(uuid: string) {
+export function isValidUUID(uuid: string) {
   // Regular expression to match RFC 4122 UUID format
   const uuidRegex =
     /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
